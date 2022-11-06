df83ab93af9e6b1b0b4411337aefc5210e3f9f54
sksharif786 <117517970+sksharif786@users.noreply.github.com>
2022-11-06 18:50:01 +0530

    added adminpanel folder

---
+++ DSIST/adminpanel/admin/pages/home.php
@@ -0,0 +1,53 @@
+
+
+<div class="app-main__outer">
+    <div id="refreshData">
+    <div class="app-main__inner">
+            <div class="app-page-title">
+                <div class="page-title-wrapper">
+                    <div class="page-title-heading">
+                       
+                    </div>
+                    <div class="page-title-actions">
+                        
+                    </div>   
+                 </div>
+            </div>            <div class="column">
+                <div class="col-md-8 col-xl-6">
+                    <div class="card mb-5 widget-content bg-midnight-bloom">
+                        <div class="widget-content-wrapper text-white">
+                            <div class="widget-content-left">
+                                <div class="widget-heading">Total Course</div>
+                                <div class="widget-subheading" style="color:transparent;">.</div>
+                            </div>
+                            <div class="widget-content-right">
+                                <div class="widget-numbers text-white">
+                                    <span><?php echo $totalCourse = $selCourse['totCourse']; ?></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8 col-xl-6">
+                    <div class="card mb-5 widget-content bg-arielle-smile">
+                        <div class="widget-content-wrapper text-white">
+                            <div class="widget-content-left">
+                                <div class="widget-heading">Total Exam</div>
+                                <div class="widget-subheading" style="color:transparent;">.</div>
+                            </div>
+                            <div class="widget-content-right">
+                                <div class="widget-numbers text-white">
+                                    <span><?php echo $totalCourse = $selExam['totExam']; ?></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+
+            <!-- <?php include("includes/graph.php"); ?> -->
+      
+        
+        </div>
+         
+    </div>
