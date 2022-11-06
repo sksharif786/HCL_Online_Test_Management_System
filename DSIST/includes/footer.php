69bd6299c98927a466396935d1870315841ad4c5
sksharif786 <117517970+sksharif786@users.noreply.github.com>
2022-11-06 18:52:21 +0530

    Add files via upload

---
+++ DSIST/includes/footer.php
@@ -0,0 +1,25 @@
+
+    </div>
+
+
+
+
+
+                <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        </div>
+    </div>
+<script type="text/javascript" src="./assets/scripts/main.js"></script></body>
+</html>
+
+
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/myjs.js"></script>
+<script type="text/javascript" src="js/ajax.js"></script>
+<script type="text/javascript" src="js/sweetalert.js"></script>
+<script type="text/javascript" src="js/facebox.js"></script>
+<!-- <script type="text/javascript" src="js/myjs.js"></script> -->
+
+
+
+
